4d1b2128943a42dbc41f4f3d766eef083c435152
warbee0712 <warbee0712@outlook.com>
2016-07-21 11:01:39 -0300

    Add webpack with config for dev environment

---
+++ internals/webpack/webpack.base.babel.js
@@ -0,0 +1,33 @@
+/**
+ *  WEBPACK BASE CONFIGURATION
+ */
+
+import path from 'path'
+
+module.exports = (options) => ({
+  context: path.join(__dirname, 'src'),
+  entry: options.entry,
+  output: Object.assign({ // Compile into js/build.js
+    path: path.resolve(process.cwd(), 'build'),
+    publicPath: '/',
+  }, options.output),
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: '/node_modules/',
+        query: options.babelQuery
+      }
+    ]
+  },
+  resolve: {
+    modules: ['app', 'node_modules'],
+    extensions: [
+      '',
+      '.js'
+    ]
+  },
+  stats: false,
+  progress: true
+})
