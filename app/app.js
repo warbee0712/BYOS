6e6440c738da5a223dfc78b4891fdcac3c51ef1f
warbee0712 <warbee0712@outlook.com>
2016-07-21 17:02:47 -0300

    Add production build and offline-plugin

--- app/app.js
+++ app/app.js
@@ -3,8 +3,14 @@ import { render } from 'react-dom'
 
 class App extends Component {
   render () {
-    return <h1>Im so awesome and bloody smart!</h1>
+    return <h1>Testing production build!</h1>
   }
 }
 
 render(<App />, document.getElementById('app'))
+
+// Install ServiceWorker and AppCache in the end since
+// it's not most important operation and if main code fails,
+// we do not want it installed
+import { install } from 'offline-plugin/runtime';
+install();
